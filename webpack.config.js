e83fd291032903088ec13a7e16ce3535773d91f5
surendarradnerus <surendarshankar333@gmail.com>
2022-12-15 23:06:45 +0530

    package change

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,85 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const path = require('path');
+const path = require('path')
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const loader = require('sass-loader')
+const CopyPlugin = require("copy-webpack-plugin")
+const ImageminWebpWebpackPlugin= require("imagemin-webp-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+
 module.exports = {
-    entry: './src/index.js',
-    output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: 'js/main.js',
-    },
     mode:'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        // filename: '[name][contenthash].js',
+        filename: '[name].js',
+        clean: true,
+        assetModuleFilename: '[name][ext]',
+    },
+    devtool:'source-map',
+    devServer:{
+        // static: {directory:path.resolve(__dirname,'dist')},
+        static: {directory:path.resolve(__dirname,'')},
+        port:3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
     module: {
-        
-        rules: [
+        rules:[
             {
-                test: /\.html$/,
-               loader: "html-loader",
+                test:/\.scss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader','sass-loader'],
             },
             {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader"
-                ]
+                test:/\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             },
-        ]
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
+
+        ],
     },
     plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
+        new HtmlWebpackPlugin({
+            title:'Umail',
+            // filename:'index.html',
+            filename:path.join(__dirname, "index.html"),
+            template: 'src/template.html',
+            scriptLoading: 'blocking',
+            favicon:'dist/favicon.ico',
+            publicPath: 'dist/'
         }),
-        new MiniCssExtractPlugin({
-            filename: 'main.css'
-        }),
+        new CopyPlugin({patterns:[
+            {from: './src/assets/sprite.svg', to:''},
+            {from: './src/assets/img', to:'./img'},
+            {from:'./src/favicon.ico', to:''},
+            // {from:'./dist/index.html', to:'../index.html'},
+        ]}),
+        new MiniCssExtractPlugin(),
+        new ImageminWebpWebpackPlugin({
+            config: [{
+              test: /\.(jpe?g|png)/,
+              options: {
+                quality:  75
+              }
+            }],
+            overrideExtension: true,
+            detailedLogs: false,
+            silent: false,
+            strict: true
+          })
     ]
 }
